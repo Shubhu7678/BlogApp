0dbbece67c29b6b9dae6398e1f22d5012f3b75a2
shubham aswal <shubhamaswal7678@gmail.com>
2025-02-01 19:35:59 +0530

    new changes

--- Frontend/src/components/core/BlogsPage/Blog.jsx
+++ Frontend/src/components/core/BlogsPage/Blog.jsx
@@ -1,8 +1,31 @@
+import { useEffect } from "react"
 
 
 const Blog = () => {
+
+  useEffect(() => { 
+         
+    const fetchBlogData = async() => { 
+       
+        
+      console.log('ejdjd');
+       
+    }
+
+    fetchBlogData();
+
+  },[])
   return (
-    <div>Blog</div>
+    <div className="bg-gray-600 w-full h-[calc(100vh-64px)] overflow-y-auto">
+      <div>
+        <div className="bg-gray-900 w-[50%]  mx-auto p-8 mt-4">
+            <h1 className="text-3xl font-bold text-gray-100">Welcome to HustleUp Blog</h1>
+            <p className="text-gray-300 text-lg">
+              Explore our blog for the latest updates, insights, and product tips.
+            </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
